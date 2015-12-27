191a31df697cbc124a1144e1942956e8f28aab0c
Raghavender Sahdev <raghavendersahdev@gmail.com>
2015-12-27 17:27:44 -0500

    Update Similarities.java

--- code/Similarities.java
+++ code/Similarities.java
@@ -8,12 +8,43 @@ import java.util.*;
  */
 public class Similarities 
 {
-	public ArrayList<String> vocabulary = new ArrayList<String>();
-	public ArrayList<String> seed_words = new ArrayList<String>();
+	public static ArrayList<String> vocabulary = new ArrayList<String>();
+	public static ArrayList<String> seed_words = new ArrayList<String>();
+	public static HashMap<String,Integer> vocabulary_map = new HashMap<String,Integer>();
+	public static HashMap<String,Integer> seed_map = new HashMap<String,Integer>();
+	
+	public static String corpusPath = "/home/sahdev/Desktop/Fall2015/july";
+	public static String word2vec_path = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/word2vec_35k.txt";
+	public static String score_file = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/scores_35k.txt";
+	
+	public static String vocab_path = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/vocabulary35k.txt";
+	public static String similarity_path = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/similarity_array2D.txt";
+	public static String vectors_path = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/vector_array2D.txt";	
+	public static String similarities_file = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/cosineSimilarities.txt";
+	
+	public static String temp_file = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/temp_file.txt";
+	public static String seed_words_path = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/glossary.txt";
+	public static int dimensionality = 100; // this is the dimensionality of the word vector
 	
 	public Similarities()
 	{
 		
+	}
+	public static void computeVocabularyMapping()
+	{
+		for(int i=0 ; i<vocabulary.size() ; i++)
+		{
+			vocabulary_map.put(vocabulary.get(i), i);
+		}
+	}
+	public static void computeSeedMapping()
+	{
+		int i;
+		for( i=0 ; i<seed_words.size() ; i++)
+		{
+			seed_map.put(seed_words.get(i), i);
+			
+		}
 	}
 	
 	/**
@@ -21,9 +52,9 @@ public class Similarities
 	 * @return this returns the seed words from the seed_words file which is glossary.txt here
 	 * @throws IOException
 	 */
-	public ArrayList<String> getSeedWords() throws IOException
+	public static void setSeedWords() throws IOException
 	{
-		String seed_words_path = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/glossary.txt";
+		//String seed_words_path = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/glossary.txt";
 		BufferedReader br2 = new BufferedReader(new FileReader(seed_words_path));
 		
 		String temp2="";
@@ -33,9 +64,26 @@ public class Similarities
 		}		
 		br2.close();
 		
-		return seed_words;
 	}
 
+	public static void setVocabulary() throws IOException
+	{		
+		BufferedReader br = new BufferedReader(new FileReader(word2vec_path));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(vocab_path));
+		String temp="";
+		String vocab_word = "";
+		while( (temp=br.readLine()) != null)
+		{
+			StringTokenizer tkn = new StringTokenizer(temp," ");
+			vocab_word = tkn.nextToken();
+			bw.write(vocab_word);
+			vocabulary.add(vocab_word);
+			bw.newLine();			
+		}		
+		bw.close();;
+		br.close();
+	}
+	
 	/**
 	 * this generates the vectorized form of the words in the vocabulary
 	 * @param row_cnt represents the number of words in the vocabulary
@@ -43,7 +91,7 @@ public class Similarities
 	 * @return returns the 2D array representing the word encodings of the words
 	 * @throws IOException
 	 */
-	public double[][] getVectors(int row_cnt,int col_cnt) throws IOException
+	public static double[][] getVectors(int row_cnt,int col_cnt) throws IOException
 	{
 		ArrayList<String> words = getDocumentWords(row_cnt);	
 		double vectors[][] = new double[row_cnt][col_cnt];
@@ -52,25 +100,25 @@ public class Similarities
 		{
 			
 			StringTokenizer str = new StringTokenizer(words.get(i)," ");
-			this.vocabulary.add(str.nextToken());
-			
+			String t = str.nextToken(); // skip the word
 			int j=0;
 			while(str.hasMoreTokens())
 			{
 				vectors[i][j] = Double.parseDouble(str.nextToken());
 				j++;
 			}
-		}
+		}		
 		
-		String vocabularyPath="/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/vocabulary.txt";
-		BufferedWriter bw = new BufferedWriter(new FileWriter(vocabularyPath));
-		for(int i=0 ; i<vocabulary.size() ; i++)
+		BufferedWriter bw = new BufferedWriter(new FileWriter(vectors_path));
+		for(int i=0 ; i<row_cnt ; i++)
 		{
-			bw.write(vocabulary.get(i));
+			for(int j=0 ; j<col_cnt ; j++)
+			{
+				bw.write(vectors[i][j]+" ");
+			}
+			bw.newLine();
 		}
-		bw.close();
-		
-		
+		bw.close();		
 		return vectors;
 	}
 	/**
@@ -79,13 +127,13 @@ public class Similarities
 	 * @return returns the words in the vocabulary with their vectorization
 	 * @throws IOException
 	 */
-	public ArrayList<String> getDocumentWords(int row_cnt) throws IOException
+	public static ArrayList<String> getDocumentWords(int row_cnt) throws IOException
 	{
-		String word2vec_path = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/word2vec.txt";
-		BufferedReader br = new BufferedReader(new FileReader(word2vec_path));
+		BufferedReader br = new BufferedReader(new FileReader(word2vec_path));	
+		HashMap<String,String> words_vectors = new HashMap<String,String>(); // maybe code this later
 		
-		ArrayList<String> words = new ArrayList<String>();
-				
+		ArrayList<String> words = new ArrayList<String>();				
+		
 		String temp = "";
 		for(int i=0; i<row_cnt; i++)
 		{
@@ -113,26 +161,34 @@ public class Similarities
 		return sim;
 	}
 
-	public double[][] getSimilarities(int seed_words_size, int vocabulary_size, double vectors[][],Similarities tester,boolean flag) throws IOException
+	public static double[][] getSimilarities(int seed_words_size, int vocabulary_size, double vectors[][],Similarities tester,boolean flag) throws IOException
 	{
 		
 		if(flag == true)
 		{
-			String similarity_path = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/similarity_array2D.txt";
 			BufferedReader br = new BufferedReader(new FileReader(similarity_path));
+			// to write code here
 			br.close();
 		}
-		String similarities_file = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/similarity_array2D.txt";
-		BufferedWriter bw = new BufferedWriter(new FileWriter(similarities_file));		
-		double similarities[][] = new double[seed_words_size][vocabulary_size];		
+		BufferedWriter bw = new BufferedWriter(new FileWriter(similarity_path));		
+		double similarities[][] = new double[seed_words_size][vocabulary_size];	
+		for(int i=0 ; i<seed_words_size ; i++)
+		{
+			for(int j=0 ; j<vocabulary_size ; j++)
+				similarities[i][j] = 0.0d;
+		}
 		for(int i=0 ; i<seed_words_size ; i++)
 		{
 			for(int j=0 ; j<vocabulary_size ; j++)
 			{
 				// find the index of the seed word
-				String str = tester.seed_words.get(i);
-				int index_seed = tester.vocabulary.indexOf(str);
-				
+				String str = seed_words.get(i);
+				if(vocabulary_map.get(str) == null)
+				{
+					//System.out.println(str + " "+i);
+					continue;
+				}	
+				int index_seed = vocabulary_map.get(str);		
 				
 				double s1[] = vectors[index_seed];
 				double v1[] = vectors[j];
@@ -156,32 +212,49 @@ public class Similarities
 	public static void main(String args[]) throws IOException
 	{
 		long time1 = System.currentTimeMillis();	
+		setSeedWords(); // here seed words = 80 from glossary.txt
+		setVocabulary();
+		computeVocabularyMapping();
+		computeSeedMapping();
 		
-		Similarities tester = new Similarities();		
-		int col_cnt = 100;   // currently hard coded
-		int row_cnt = 28916;  // currently hard coded
+		Similarities tester = new Similarities();	
 		
-		double vectors[][] = new double[row_cnt][col_cnt];
-		vectors = tester.getVectors(row_cnt, col_cnt);	
-		tester.seed_words = tester.getSeedWords();
+		int vocabulary_size = vocabulary.size();
+		int seed_words_size = seed_words.size();
+		int vocab_map = vocabulary_map.size();
+		int seed_map2 = seed_map.size();
+		
+		System.out.println(vocabulary_size);
+		System.out.println(seed_words_size);
+		System.out.println(vocab_map);
+		System.out.println(seed_map2);
+		
+		int col_cnt = dimensionality;   // currently hard coded
+		int row_cnt = vocabulary_size;  // currently hard coded not now its 17454 here
+		
+		
+		
+		double vectors[][] = new double[row_cnt][col_cnt];		
+		vectors = getVectors(row_cnt, col_cnt);	
+		
 		/* till here we have 
 		 * vectors[][] - the vector representations of words stored in the 2d array
 		 * seed_words - ArrayList<String> has all the seed words read from the glossary
 		 * vocabulary - ArrayList<String> has the vocabulary generated from the word2vec file path
-		 *  
+		 * vocabulary_map
+		 * seed_map
 		 * now we need to compute the similarities between seed words and document words
 		 */				
 		// Start computing the seed words similarity
 		
-		int vocabulary_size = tester.vocabulary.size();
-		int seed_words_size = tester.seed_words.size();
 		
 		
 		double similarities[][] = new double[seed_words_size][vocabulary_size];
 		// $$$$$$$$ uncomment the following function call FOR FINAL TESTING
-		similarities = tester.getSimilarities(seed_words_size, vocabulary_size, vectors, tester, false);
+		similarities = getSimilarities(seed_words_size, vocabulary_size, vectors, tester, false);
 		
-		String similarities_file = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/cosineSimilarities.txt";
+		
+		
 		BufferedWriter bw = new BufferedWriter(new FileWriter(similarities_file));
 		for(int i=0 ; i<seed_words_size ; i++)
 		{
@@ -197,13 +270,13 @@ public class Similarities
 		
 		// start computing the document scores
 		// read file paths below
-		String corpusPath = "/home/sahdev/Desktop/Fall2015/samples";
+		//String corpusPath = "/home/sahdev/Desktop/Fall2015/tester1";
 		File folder = new File(corpusPath);
 		File[] listOfFiles = folder.listFiles();
 		ArrayList<String> filePaths = new ArrayList<String>();
-		//int NUM_FILES = listOfFiles.length;
-		int NUM_FILES = 10000;
-		
+		int NUM_FILES = listOfFiles.length;
+		//int NUM_FILES = 1000;
+		System.out.println("Number of Files: "+NUM_FILES);
 		for(int i=0 ; i<NUM_FILES ; i++)
 		{
 			if(listOfFiles[i].isFile())
@@ -226,7 +299,7 @@ public class Similarities
 		
 		
 		// remove null files below and re-adjust NUM_FILES
-		String temp_file = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/temp_file.txt";
+		//String temp_file = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/temp_file.txt";
 		BufferedWriter bw3 = new BufferedWriter(new FileWriter(temp_file));
 		int nulls = 0;
 		ArrayList<String> content2 = new ArrayList<String>();
@@ -267,9 +340,9 @@ public class Similarities
 		
 		
 		
-		String score_file = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/scores.txt";
+		
 		BufferedWriter bw2 = new BufferedWriter(new FileWriter(score_file));
-		for(int i=0 ; i<3000 ; i++)
+		for(int i=0 ; i<NUM_FILES ; i++)
 		{		
 			int temp_wrd_cnt=0;
 
@@ -281,13 +354,14 @@ public class Similarities
 				{
 					String doc_word = tkn.nextToken();
 					
-					int word_index = tester.vocabulary.indexOf(doc_word);
-					int seed_index = tester.seed_words.indexOf(tester.seed_words.get(j));
+					if(vocabulary_map.get(doc_word) == null || seed_map.get(seed_words.get(j)) == null)
+						continue;
+					int word_index = vocabulary_map.get(doc_word);//vocabulary.indexOf(doc_word);
+					int seed_index = seed_map.get(seed_words.get(j));//seed_words.indexOf(seed_words.get(j));
 					if(word_index <0 || seed_index<0)
 						continue;
 					
 					score[i] = score[i] + similarities[seed_index][word_index];
-					;
 				}
 			}
 			//System.out.println(" "+score[i]);
@@ -295,7 +369,6 @@ public class Similarities
 			score[i] = score[i] / temp_wrd_cnt;
 			bw2.write(score[i]+" "+date2.get(i));
 			bw2.newLine();
-			System.out.println(i);
 		}
 		
 		bw2.close();
@@ -303,23 +376,5 @@ public class Similarities
 		long time3 = System.currentTimeMillis();
 		System.out.println("Time taken: "+(time3-time1));
 		
-		//computing 
-		
-		
-		
-		/*
-		String temp_path = "/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/word2vec_test.txt";
-		BufferedWriter bw2 = new BufferedWriter(new FileWriter(temp_path));
-		for(int i=0 ; i<row_cnt ; i++)
-		{
-			for(int j=0 ; j<col_cnt ; j++)
-			{
-				bw2.write(""+vectors[i][j]);
-			}
-			bw2.newLine();
-		}
-		bw2.close();
-		*/
-	}
-	
+	}	
 }
