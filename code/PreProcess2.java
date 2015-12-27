7d50f45e872613687b588b642ce3f59647542487
Raghavender Sahdev <raghavendersahdev@gmail.com>
2015-12-27 17:30:29 -0500

    Update PreProcess2.java

--- code/PreProcess2.java
+++ code/PreProcess2.java
@@ -14,16 +14,18 @@ public class PreProcess2
 	 * @param args
 	 * @throws IOException
 	 */
+	public static String corpusPath = "/home/sahdev/Desktop/Fall2015/nov";
+	public static String output_file ="/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/output_content_312k.txt";
 	public static void main(String args[]) throws IOException
 	{	
 		long time1 = System.currentTimeMillis();
 		
-		String corpusPath = "/home/sahdev/Desktop/Fall2015/samples";
+		
 		File folder = new File(corpusPath);
 		File[] listOfFiles = folder.listFiles();
 		ArrayList<String> filePaths = new ArrayList<String>();
-		//int NUM_FILES = listOfFiles.length;
-		int NUM_FILES = 10000;
+		int NUM_FILES = listOfFiles.length;
+		//int NUM_FILES = 7914;
 		
 		for(int i=0 ; i<NUM_FILES ; i++)
 		{
@@ -37,6 +39,10 @@ public class PreProcess2
 		
 		String temp="";
 		String whole_content="";
+		
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter(output_file));
+		
 		for(int i=0; i<filePaths.size() ; i++)
 		{
 			String temp_path = filePaths.get(i);
@@ -47,12 +53,12 @@ public class PreProcess2
 				whole_content = whole_content+temp+" ";
 			}
 			whole_content = whole_content+"\n";
+			bw.write(whole_content);
+			whole_content = "";
 			br.close();
 		}
-		temp = "fdsfD";
-		String output_file ="/home/sahdev/Desktop/Fall2015/Data Mining/PROJECT/output_file.txt";
-		BufferedWriter bw = new BufferedWriter(new FileWriter(output_file));
-		bw.write(whole_content);
+		
+		
 		bw.close();
 		
 		long time2 = System.currentTimeMillis();
